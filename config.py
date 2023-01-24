64a30f8017a71054d8a73a55f4b79db3188a216f
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2023-01-24 04:44:05 +0400

    update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5875169901:AAGCA2P9awld_86WY5VecA-1gKk-xdYgEmc")
+    API_ID = int(os.environ.get("API_ID", "19485442"))
+    API_HASH = os.environ.get("API_HASH", "a03fcb372b3ec4e406b5d52f84b02e53")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "nesirovqadirofficiall")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ayparasongbot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "qrupplaylist")
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001719261837"))
